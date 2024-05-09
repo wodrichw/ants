bede372ea7b147b1cb71a3d017530da3f3f972ed
Kevin Chevalier <kevin.chevalier@siemens.com>
2024-05-09 13:24:55 -0700

    add op index to serializer

--- src/core/hardware/program_executor.cpp
+++ src/core/hardware/program_executor.cpp
@@ -1,18 +1,21 @@
 #include "hardware/program_executor.hpp"
 
-#include <string>
-
-#include "app/globals.hpp"
 #include "entity/entity_data.hpp"
-#include "entity/map.hpp"
+#include "proto/hardware.pb.h"
 #include "spdlog/spdlog.h"
+#include "utils/serializer.hpp"
 
-ProgramExecutor::ProgramExecutor(Unpacker&) {}
+ProgramExecutor::ProgramExecutor(Unpacker& p) {
+    ant_proto::ProgramExecutor msg;
+    p >> msg;
+
+    op_idx = msg.op_idx();
+}
 
 void ProgramExecutor::handleClockPulse() {
     // SPDLOG_TRACE("Handling clock pulse for program_executor");
     bool op_result = true;
-    for (int i = 0; i < 500 && op_idx < _ops.size() && op_result; ++i) {
+    for(int i = 0; i < 500 && op_idx < _ops.size() && op_result; ++i) {
         op_result = _ops[op_idx]();
         ++op_idx;
         SPDLOG_TRACE("Incrementing op_idx to {}", op_idx);
@@ -21,6 +24,8 @@ void ProgramExecutor::handleClockPulse() {
     // SPDLOG_TRACE("Clock pulse handled for program_executor");
 }
 
-Packer& operator<<(Packer& p, ProgramExecutor const&) {
-    return p; 
+Packer& operator<<(Packer& p, ProgramExecutor const& obj) {
+    ant_proto::ProgramExecutor msg;
+    msg.set_op_idx(obj.op_idx);
+    return p << msg;
 }
