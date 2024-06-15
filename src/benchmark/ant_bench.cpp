909131523ac3e3ebb06a145cd32a098f6c956bcd
Kevin Chevalier <25399619+kchevali@users.noreply.github.com>
2024-06-14 20:04:33 -0600

    counter 20000 benchmark

--- src/benchmark/ant_bench.cpp
+++ src/benchmark/ant_bench.cpp
@@ -7,7 +7,7 @@ static void run_save(benchmark::State& state, std::string const& filename) {
     ProjectArguments config("", "../assets/saves/" + filename, false, false, true);
     AntGameFacade game(config);
     for(auto _ : state) {
-        for(ulong i = 0; i < 60; ++i) {
+        for(ulong i = 0; i < 600; ++i) {
             game.engine_update();
         }
     }
@@ -19,7 +19,11 @@ static void run_small(benchmark::State& state) {
 static void run_mid(benchmark::State& state) {
     run_save(state, "mid_sq");
 }
+static void run_counter(benchmark::State& state) {
+    run_save(state, "count");
+}
 BENCHMARK(run_small);
 BENCHMARK(run_mid);
+BENCHMARK(run_counter);
 
 BENCHMARK_MAIN();
